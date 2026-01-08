db093a11859d38691d3ce6128a493046f2c99a9f
JUAN CAMILO RAMIREZ DIAZ <juancaradi@gmail.com>
2026-01-07 23:26:20 -0500

    Add custom domain CNAME and update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Tu dirección web final
-  site: 'https://juancaradi.github.io',
+  site: 'https://camilomath.xyz',
   
   // Como es tu repositorio principal, la base es la raíz '/'
   base: '/',
